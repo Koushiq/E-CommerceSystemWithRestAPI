81b5921ffc529ae2dd728275f259752283cc8063
17-33441-1@student.aiub.edu <17-33441-1@student.aiub.edu>
2021-01-02 23:58:07 +0600

    Add project files.

---
+++ Models/WishlistItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace E_CommerceSystemWithRestAPI.Models
+{
+    public class WishlistItem
+    {
+        public int WishlistItemId { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
